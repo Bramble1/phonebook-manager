d3af4e17dd2125666e50495e8c2293b32cec20a8
Sam D <24442697+digitalXmage@users.noreply.github.com>
2023-03-30 14:30:38 +0100

    Add files via upload

--- lab/investment.py
+++ lab/investment.py
@@ -1,12 +1,12 @@
 import sys
 
-def calculate_interest(investment,interest_rate):
+def calculate_interest(investment,interest_rate,target_value):
     if not(interest_rate>=1 and interest_rate<=100):
         print("interest rate must be a number between 1-100(percentage)")
         exit()
     
     years=0
-    while(investment<1000):
+    while(investment<target_value):
         result=(interest_rate*(100**-1))*investment
         investment+=result
         years+=1
@@ -15,6 +15,6 @@ def calculate_interest(investment,interest_rate):
 
 
 #main
-#investment,interest_rate
-calculate_interest(100,10)
+#investment,interest_rate,target_value
+calculate_interest(100,10,1000)
 
